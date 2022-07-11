96980f4693b8c7ea277e27f6dda3ab2818df19fc
10706064 <10706064@LTILMAA21202757.lntinfotech.com>
2022-07-11 12:52:39 +0530

    product commit

---
+++ src/main/java/com/shopping/review/model/Review.java
@@ -0,0 +1,25 @@
+package com.shopping.review.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+	
+	   @Id
+	   @GeneratedValue(strategy = GenerationType.IDENTITY)
+	   private Long reviewId;
+	   private String productId;
+	   private String username;
+	   private String review;
+
+}
